6ce4f01ef70b82b8639603b81e9f5a406ea4c740
jep1220 <38770396+jep1220@users.noreply.github.com>
2021-05-09 22:47:03 -0700

    components update

--- src/components/DataBody.js
+++ src/components/DataBody.js
@@ -1,8 +1,8 @@
 import React from "react";
-import "../styles/DataBody.css";
+//import "../styles/DataBody.css";
 
 function DataBody({ users }) {
-  function formatDate(date) {
+    function formatDate(date) {
     const dateArray = date.split("-");
     const year = dateArray[0];
     const month = dateArray[1];
@@ -10,43 +10,43 @@ function DataBody({ users }) {
     const day = dayArray[0];
     const formattedDate = [month, day, year].join("-");
     return formattedDate;
-  }
+    }
 
-  return (
-    <tbody>
-      {users[0] !== undefined && users[0].name !== undefined ? (
-        users.map(({ login, name, picture, phone, email, dob }) => {
-          return (
-            <tr key={login.uuid}>
-              <td data-th="Image" className="align-middle">
-                <img
-                  src={picture.medium}
-                  alt={"profile image for " + name.first + " " + name.last}
-                  className="img-responsive"
-                />
-              </td>
-              <td data-th="Name" className="name-cell align-middle">
-                {name.first} {name.last}
-              </td>
-              <td data-th="Phone" className="align-middle">
-                {phone}
-              </td>
-              <td data-th="Email" className="align-middle">
-                <a href={"mailto:" + email} target="__blank">
-                  {email}
-                </a>
-              </td>
-              <td data-th="DOB" className="align-middle">
-                {formatDate(dob.date)}
-              </td>
-            </tr>
-          );
-        })
-      ) : (
-        <></>
-      )}
-    </tbody>
-  );
+    return (
+        <tbody>
+            {users[0] !== undefined && users[0].name !== undefined ? (
+                users.map(({ login, name, picture, phone, email, dob }) => {
+                    return (
+                        <tr key={login.uuid}>
+                            <td data-th="Image" className="align-middle">
+                                <img 
+                                    src={picture.medium}
+                                    alt={"profile image for " + name.first +  " " + name.last}
+                                    className="img-responsive rounded"
+                                />
+                            </td>
+                            <td data-th="Name" className="name-cell align-middle">
+                                {name.first} {name.last}
+                            </td>
+                            <td data-th="Phone" className="align-middle">
+                                {phone}
+                            </td>
+                            <td data-th="Email" className="align-middle">
+                                <a href={"mailto:" + email} target="_blank">
+                                    {email}
+                                </a>
+                            </td>
+                            <td data-th="DOB" className="align-middle">
+                                {formatDate(dob.date)}
+                            </td>
+                        </tr>
+                    );
+                })
+            ) : (
+                <></>
+            )}
+        </tbody>
+    );
 }
 
 export default DataBody;
--- src/components/Wrapper.js
+++ src/components/Wrapper.js
@@ -1,12 +1,13 @@
-import React from 'react';
-import "../styles/Wrapper.css";
+import React from "react";
+//import "../styles/Wrapper";
 
-function Wrapper({ children }) {
-  return (
-      <div className="wrapper">
-        { children }
-      </div>
-  );
+function Wrapper({children}){
+
+    return (
+        <div className="wrapper">
+            {children}
+        </div>
+    );
 }
 
 export default Wrapper;
